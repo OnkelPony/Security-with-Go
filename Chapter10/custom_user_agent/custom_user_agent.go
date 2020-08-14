eb2681191e3d4921282523c6d45655ab3bc47adc
Jiří Martínek <zwo@centrum.cz>
2020-08-15 00:11:51 +0200

    Chapter 10 - before Using the goquery package for web scraping

--- Chapter10/custom_user_agent/custom_user_agent.go
+++ Chapter10/custom_user_agent/custom_user_agent.go
@@ -9,13 +9,13 @@ import (
 func main() {
 	// Create the request for use later
 	client := &http.Client{}
-	request, err := http.NewRequest("GET", "https://www.devdungeon.com", nil)
+	request, err := http.NewRequest("GET", "http://tessafowler.gam.as", nil)
 	if err != nil {
 		log.Fatal("Error creating request. ", err)
 	}
 
 	// Override the user agent
-	request.Header.Set("User-Agent", "_Custom User Agent_")
+	request.Header.Set("User-Agent", "ש״ב")
 
 	// Perform the request, ignore response.
 	_, err = client.Do(request)
